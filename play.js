fb5aa4aeaed0a3d3d01745b22a77cabd353ef6bc
Hawa <hawa.mjh@gmail.com>
2023-04-29 18:01:46 +0000

    more changed with play.js and client.js

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.2.15', // IP address here
-    port: 50541 // PORT number here
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // handle incoming data from server
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
